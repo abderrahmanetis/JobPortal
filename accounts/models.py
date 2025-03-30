ddfa934462cc4d2144ae0fb46fa44c8126dc582d
tis Abderrahmane <tisabdo7@gmail.com>
2025-03-30 11:46:02 +0000

    Initial commit

---
+++ accounts/models.py
@@ -0,0 +1,30 @@
+import uuid
+from datetime import datetime, timezone
+
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+from common.models import BaseModel
+
+from .manager import CustomUserManager
+
+class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+class PendingUser(BaseModel):  # ✅ Cette classe est maintenant bien définie
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    verification_code = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_valid(self) -> bool:
+        lifespan_in_seconds = 20 * 60
+        now = datetime.now(timezone.utc)
+        timediff = now - self.created_at
+        timediff = timediff.total_seconds()
+        return timediff <= lifespan_in_seconds  # Simplification du return
---
+++ accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render ,redirect
+from django.http import HttpRequest
+from .models import User , PendingUser
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
+from django.utils.crypto import get_random_string
+from datetime import  datetime , timezone
+def register(request: HttpRequest):
+    if request.method == "POST":
+        email: str = request.POST["email"]
+        password: str = request.POST["password"]
+        cleaned_email = email.lower()
+
+        if User.objects.filter(email=cleaned_email).exists():
+            messages.error(request, "Email exists on the platform")
+            return redirect("register")
+
+        else:
+            verification_code = get_random_string(10)
+            PendingUser.objects.update_or_create(
+                email=cleaned_email,
+                defaults={
+                    "password": make_password(password),
+                    "verification_code": verification_code,
+                    "created_at": datetime.now(timezone.utc),
+                },
+            )
+            # send_email.delay(
+            #     "Verify Your Account",
+            #     [cleaned_email],
+            #     "emails/email_verification_template.html",
+            #     context={"code": verification_code},
+            # )
+            messages.success(request, f"Verification code sent to {cleaned_email}")
+            return render(
+                request, "verify_account.html"
+            )
+
+    else:
+        return render(request, "register.html")
